78637e5bd0df48829bd8426c00311627292d4242
Luigi Genovese <luigi.genovese@cea.fr>
2018-04-12 12:19:19 +0200

    v_sim run script updated

--- run_v-sim.sh
+++ run_v-sim.sh
@@ -5,4 +5,4 @@ xhost +local:root #requires x11-xserver-utils
 docker run -ti  \
     -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $PWD:/$dirname -w /$dirname \
-    v_sim-dev v_sim-dev $*
+    luigigenovese/v_sim-dev v_sim-dev $*
