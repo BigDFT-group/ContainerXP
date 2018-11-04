7a205467d54aeb0909a6eb7b84a7fca03a0d7fa7
Luigi <luigi.genovese@cea.fr>
2018-11-04 16:02:26 +0100

    add script to install docker-ce on a machine

---
+++ install_docker-ce.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+sudo apt update
+
+sudo apt --yes install \
+    software-properties-common \
+    apt-transport-https \
+    ca-certificates \
+    curl
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt update
+
+sudo apt --yes install docker-ce
+
+sudo usermod --append --groups docker $USER
+
