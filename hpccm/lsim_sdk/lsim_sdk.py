60fc5b5368c502f9868a045f202e8e2b14d8ee8b
Augustin Degomme <adegomme@users.noreply.github.com>
2021-09-20 23:16:53 +0200

    new version of the SDK, split in three python scripts. Can now specify the toolchain, the mpi flavour, the python flavour, the blas flavour, use oneapi or cuda as basis

---
+++ hpccm/lsim_sdk/lsim_sdk.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import logging
+
+import hpccm
+import hpccm.config
+from arguments import arguments, doc, footer
+from hpccm.building_blocks import *
+from hpccm.primitives import *
+
+def sdk():
+  args, distro = arguments()
+
+  tdoc=doc(args, 'sdk')
+
+  #######
+  ## SDK stage
+  #######
+  # Set the image tag based on the specified version (default to 10.0)
+  mkl = 'no'
+  if args.cuda != 'no':
+    image = 'nvidia/cuda:{}-devel-{}{}'.format(args.cuda, args.system, args.system_version)
+    if args.oneapi != 'no':
+      oneapi = 'no'
+      logging.warning('For now we can\'t mix CUDA SDK with OneAPI base image. MKL can still be installed later. Ignoring OneAPI at this step')  
+  elif args.oneapi != 'no':
+    image = 'intel/oneapi-hpckit:devel-{}{}'.format(args.system, args.system_version)
+    if args.target_arch != 'x86_64':
+      logging.error('OneAPI is only valid for amd64 processors')
+  else:
+    image = '{}:{}'.format(args.system, args.system_version if args.system_version is not None else 'latest')
+
+  Stage0 = hpccm.Stage()
+
+  Stage0 += comment(tdoc, reformat=False)
+  Stage0.name = 'sdk'
+  Stage0.baseimage(image,_distro=distro)
+  Stage0 += comment('SDK stage', reformat=False)
+
+  Stage0 += label(metadata={'maintainer': 'bigdft-developers@lists.launchpad.net'})
+  Stage0 += environment(variables={'DEBIAN_FRONTEND': 'noninteractive'})
+  #SHELL ['/bin/bash', '-c']
+  Stage0 += raw(docker='SHELL ["/bin/bash", "-c"]')
+  Stage0 += shell(commands=['useradd -ms /bin/bash lsim'])
+
+  #BigDFT packages
+  #system independent ones
+  ospackages=['autoconf', 'automake', 'bison', 'bzip2', 'chrpath', 'cmake', 'cpio', 'curl', 'doxygen', 'ethtool', 'flex',
+              'gdb', 'gettext', 'git', 'gnome-common', 'graphviz', 'intltool', 'kmod', 'libtool', 'lsof', 'net-tools', 'ninja-build',
+              'patch', 'pciutils', 'perl', 'pkg-config', 'rsync', 'strace', 'swig', 'tcl', 'tk', 'valgrind', 'vim', 'wget']
+
+  apt_packages=ospackages+[
+          'autotools-dev', 'libpcre3-dev', 'libltdl-dev', 'lsb-release', 'libz-dev', 'zlib1g-dev', 'libzmq-dev', 'libmount-dev',
+          'iproute2', 'libnl-route-3-200', 'libnuma1', 'linux-headers-generic', 'gtk-doc-tools', 'libxml2-dev', 'libglu1-mesa-dev',
+          'libnetcdf-dev', 'libgirepository1.0-dev', 'dpatch', 'libgtk-3-dev', 'libmount-dev', 'locales', 'ssh']
+  yum_packages=ospackages+['pcre-devel', 'libtool-ltdl-devel', 'redhat-lsb', 'glibc-devel', 'zlib-devel', 'zeromq-devel', 'libmount-devel', 
+          'iproute', 'libnl3-devel', 'numactl-libs', 'kernel-headers', 'gtk-doc', 'libxml2-devel', 'mesa-libGLU-devel', 'netcdf-devel',
+          'gobject-introspection-devel',  'gtk3-devel', 'libmount-devel', 'openssh', 'libarchive']
+
+  if args.cuda != 'no':
+    apt_packages += ['ocl-icd-libopencl1']
+    yum_packages += ['ocl-icd']
+    Stage0 += environment(variables={ 'LD_LIBRARY_PATH': '/usr/local/lib:/usr/local/cuda/lib64:${LD_LIBRARY_PATH}',
+                                      'LIBRARY_PATH': '/usr/local/cuda/lib64:${LIBRARY_PATH}',
+                                      'NVIDIA_VISIBLE_DEVICES': 'all',
+                                      'NVIDIA_DRIVER_CAPABILITIES': 'compute,utility'})
+    Stage0 += shell(commands=['mkdir -p /etc/OpenCL/vendors',
+                              'echo "libnvidia-opencl.so.1" > /etc/OpenCL/vendors/nvidia.icd',
+                              'cp /usr/local/cuda/lib64/stubs/libcuda.so /usr/local/lib/libcuda.so.1',
+                              'cp /usr/local/cuda/lib64/stubs/libnvidia-ml.so /usr/local/lib/libnvidia-ml.so.1'])
+
+  Stage0 += packages(apt=apt_packages, yum=yum_packages, powertools=True, epel=True)
+
+  if args.target_arch == 'x86_64':
+
+    conda_packages = [ 'openbabel', 'six', 'matplotlib', 'ipython',
+                      'nbval', 'cython', 'sphinx', 'sphinx_bootstrap_theme', 
+                      'watchdog', 'sphinx_rtd_theme', 'flake8', 'ncurses', 'pygobject']
+    channels=['conda-forge', 'nvidia']
+    if args.blas == 'mkl' and args.oneapi == 'no':
+      conda_packages += ['mkl-devel']
+      Stage0 += environment(variables={'MKLROOT': '/usr/local/anaconda/'})
+
+    if args.python == 'intel' and args.oneapi == 'no':
+      conda_packages += ['intelpython3_core']
+      channels += ['intel']
+
+    if args.jupyter == 'yes':
+      conda_packages += [ 'jupyterlab', 'ipykernel']
+    
+    #conda install
+    if args.oneapi == 'no':
+      Stage0 += conda(version='4.10.3', python_subversion='py37', channels=channels,
+                    eula=True, packages=conda_packages)
+      conda_path = '/usr/local/anaconda/'
+      commands = ['groupadd conda',
+                  'usermod -a -G conda lsim',
+                  'chgrp -R conda ' + conda_path,
+                  'chmod -R 770 ' + conda_path]
+    else:
+      #use already present conda on oneapi images
+      conda_path = '/opt/intel/oneapi/intelpython/latest/'
+      commands = ['conda config --add channels ' + ' --add channels '.join(channels),
+        'conda install -y '+ ' '.join(conda_packages),
+        'conda clean -afy' ]
+
+    if args.python == 'intel':
+      commands += ['ln -s ' + conda_path + 'bin/python3-config' + conda_path + '/bin/python-config']
+      #Intel python forgets to provide ncurses https://community.intel.com/t5/Intel-Distribution-for-Python/curses-missing-on-python-3-7/m-p/1201384#M1509
+      #Temporarily steal the files from conda-forge package, and use them instead, as it's used in bigdft-tool.
+      commands += ['mkdir curses',
+                  'cd curses',
+                  'wget https://anaconda.org/conda-forge/python/3.7.8/download/linux-64/python-3.7.8-h6f2ec95_1_cpython.tar.bz2',
+                  'tar xjf python-3.7.8-h6f2ec95_1_cpython.tar.bz2',
+                  'cp ./lib/python3.7/lib-dynload/_curses* ' + conda_path + 'lib/python3.7/lib-dynload/',
+                  'cd ..',
+                  'rm -rf curses']
+    Stage0 += shell(commands=commands)
+
+    #update LIBRARY_PATH as well to allow building against these libs :
+    Stage0 += environment(variables={'PATH':  conda_path + '/bin:$PATH',
+                                     'LIBRARY_PATH': conda_path + 'lib/:${LIBRARY_PATH}'})
+    python_path = conda_path
+
+  else:
+    #on other platforms miniconda is not available. Use system python and libraries
+    ospack=[
+    'python3', 'python3-flake8', 'python3-pip', 'python3-matplotlib',
+    'python3-six', 'python3-sphinx', 'python3-sphinx-bootstrap-theme',
+    'python3-scipy', 'python3-numpy', 'watchdog', 'python3-ipython',
+    'python3-flake8']
+    yum=ospack+['python3-Cython', 'python3-sphinx_rtd_theme']
+    apt=ospack+['cython3', 'python3-sphinx-rtd-theme', 'python-is-python3']
+    if args.jupyter == 'yes':
+      apt+=['jupyter-notebook', 'python3-ipykernel']
+    Stage0 += packages(apt=apt, yum=yum, powertools=True, epel=True)
+
+    #somehow there is no jupyter package for centos 8.
+    if args.system == 'centos' and args.jupyter == 'yes':
+      #make python3 and pip3 default
+      Stage0 += shell(commands=['ln -s /usr/bin/python3 /usr/local/bin/python',
+                              'ln -s /usr/bin/pip3 /usr/local/bin/pip',
+                              'pip install jupyter ipykernel'])
+    python_path = '/usr/local'
+
+  #Install boost with the provided python
+  Stage0 += shell(commands=[ 'echo "\\\n\
+    using python\\\n\
+    : \\\n\
+    : '+python_path+'/bin/python\\\n\
+    : `dirname '+python_path+'/include/python*/..`\\\n\
+    : '+python_path+'/lib\\\n\
+    ;\\\n\
+" > /tmp/user-config.jam' ])
+  Stage0+= boost(python=args.python != 'no', 
+                 bootstrap_opts=['--with-libraries=python,serialization', '--with-python=`which python`', '--without-icu'],
+                 b2_opts=['--user-config=/tmp/user-config.jam', 'install', 'threading=multi', 'variant=release', 'link=shared', 'stage', '--with-regex', '--disable-icu', '--with-thread', '--with-serialization', '--with-iostreams', '--with-python', '--with-system', '--with-test', '-q']) 
+
+  if (args.jupyter == 'yes'):
+    Stage0 += raw(docker='EXPOSE 8888')
+    Stage0 += raw(docker='CMD jupyter lab --ip=0.0.0.0 --allow-root --NotebookApp.token=bigdft --no-browser', 
+                  singularity='%runscript\n jupyter lab --ip=0.0.0.0 --allow-root --NotebookApp.token=bigdft --no-browser')
+
+  if args.system == 'ubuntu' and args.system_version >= StrictVersion('20.04') and target_arch == "x86_64":
+    Stage0 += environment(variables={'LD_PRELOAD': '/usr/lib/x86_64-linux-gnu/libtinfo.so.6'})
+
+  if args.system == 'ubuntu':
+    Stage0 += environment(variables={ "LANG": "en_US.UTF-8",
+                                      "LANGUAGE": "en_US.UTF-8",
+                                      "LC_ALL": "en_US.UTF-8"})
+  else:
+    Stage0 += environment(variables={ "LANG": "C.UTF-8",
+                                      "LANGUAGE": "C.UTF-8",
+                                      "LC_ALL": "C.UTF-8",
+                                      "PKG_CONFIG_PATH": "/usr/lib64:/usr/share/lib64"})   
+
+  return Stage0
+
+if __name__ == '__main__':
+  print(footer(sdk()))
---
+++ hpccm/lsim_sdk/lsim_toolchain.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from distutils.version import LooseVersion, StrictVersion
+import logging
+
+import hpccm
+import hpccm.config
+from arguments import arguments, doc, footer
+from hpccm.building_blocks import *
+from hpccm.primitives import *
+
+def toolchain():
+  Stage0 = hpccm.Stage()
+
+  args, distro = arguments()
+
+  tdoc = doc(args, "toolchain")
+
+  Stage0 += comment(tdoc, reformat=False)
+  Stage0.name = 'toolchain'
+  test = hpccm.config.g_cpu_arch
+  Stage0.baseimage(image='sdk',_distro=distro)
+  hpccm.config.set_cpu_architecture(args.target_arch)
+
+  Stage0 += comment("Toolchain stage", reformat=False)
+  tc = None
+  if args.toolchain == 'gnu':
+    # GNU compilers
+    tgnu = gnu(version=args.toolchain_version, ldconfig=True)
+    # gcc is usually already present and update-alternatives will fail
+    if args.system == 'centos' and args.toolchain_version is not None:
+      Stage0 += environment(variables={
+        "PATH": "/opt/rh/gcc-toolset-"+args.toolchain_version+"/root/usr/bin:${PATH}}",
+        "LD_LIBRARY_PATH":"/opt/rh/gcc-toolset-"+args.toolchain_version+"/root/lib/gcc/"+args.target_arch+"-redhat-linux/"+args.toolchain_version+"/:${LD_LIBRARY_PATH}",
+        "LIBRARY_PATH":"/opt/rh/gcc-toolset-"+args.toolchain_version+"/root/lib/gcc/"+args.target_arch+"-redhat-linux/"+args.toolchain_version+"/:${LIBRARY_PATH}"
+      })
+
+    Stage0 += tgnu
+    tc = tgnu.toolchain
+  elif args.toolchain == 'llvm':
+  
+    tllvm = llvm(version=args.toolchain_version, ldconfig=True)
+    Stage0 += tllvm
+    tc = tllvm.toolchain
+    Stage0 += packages(ospackages=['gfortran'], powertools=True, epel=True)
+    tc.FC = 'gfortran'
+    """ elif toolchain == 'intel' and oneapi == 'no':
+      intel_license = USERARG.get('intel_license', None)
+      if intel_license is None:
+        logging.error("Intel license file or server must be provided to support intel toolchain, please provide intel_license userarg.")
+      intel = intel_psxe(eula=True, license=intel_license)
+      Stage0 += intel
+      tc = intel.toolchain """
+  elif args.toolchain == 'arm' and "arm" in args.target_arch:
+    if args.toolchain_version is None:
+      args.toolchain_version = "20.3"
+    arm = arm_allinea_studio(eula=True, microarchitectures=['generic', 'thunderx2t99', 'generic-sve'], version=args.toolchain_version)
+    Stage0 += arm
+    tc = arm.toolchain
+  elif args.toolchain == 'intel' or args.oneapi != 'no':
+    tc = hpccm.toolchain(CC='icc', CXX='icpc', F77='ifort',
+                                   F90='ifort', FC='ifort')
+  
+  if args.cuda != "no":
+    tc.CUDA_HOME = '/usr/local/cuda'
+
+  if args.blas != 'mkl' and args.blas !='openblas':
+    apt_packages = ['libblas-dev', 'liblapack-dev']
+    yum_packages = ['blas-devel', 'lapack-devel']
+    Stage0 += packages(apt=apt_packages, yum=yum_packages, powertools=True, epel=True)
+  elif args.blas == 'openblas':
+    #build and install with default optims
+    Stage0+=openblas(version="0.3.17", ldconfig=True, toolchain=tc, environment=True)
+    #add AVX2 and AVX512 versions
+    if args.target_arch == "x86_64":
+      Stage0+=openblas(version="0.3.17", ldconfig=False, toolchain=tc, environment=False, make_opts=['TARGET=HASWELL', 'USE_OPENMP=1', 'CROSS=1'], prefix='/var/tmp/haswell')
+      Stage0+=shell(commands=['mkdir -p /usr/local/openblas/lib/haswell',
+                              'mv /var/tmp/haswell/lib /usr/local/openblas/lib/haswell',
+                              'rm -rf /var/tmp/haswell'])
+      Stage0+=openblas(version="0.3.17", ldconfig=False, toolchain=tc, environment=False, make_opts=['TARGET=SKYLAKEX', 'USE_OPENMP=1', 'CROSS=1'], prefix='/var/tmp/avx')
+      Stage0+=shell(commands=['mkdir -p /usr/local/openblas/lib/avx512_1',
+                              'mv /var/tmp/avx/lib /usr/local/openblas/lib/avx512_1',
+                              'rm -rf /var/tmp/avx'])
+  return Stage0, tc
+
+if __name__ == '__main__':
+  from lsim_sdk import sdk
+  print(sdk()) 
+  print(footer(toolchain()))
